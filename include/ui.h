c9df9a81558fdd95797acea798b9449746d503c6
Abhishek <abhihivarkar783@gmail.com>
2026-07-04 05:46:47 +0000

    Implement menu-driven user interface for dynamic array

--- include/ui.h
+++ include/ui.h
@@ -1,8 +1,9 @@
 #ifndef UI_H
 #define UI_H
 
+#include "dynamic_array.h"
 void menu_option(void);
-void add_value(void);
-void start_ui(void);
-
+void add_value(DynamicArray *arr);
+void start_ui(DynamicArray *arr);
+void add_element_at(DynamicArray *arr);
 #endif
--- src/dynamic_array/ui.c
+++ src/dynamic_array/ui.c
@@ -1,39 +1,73 @@
-#include "ui.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "dynamic_array.h"
+#include <stdio.h>
+
 void menu_option(void) {
- printf("========= Welcome =========\n");
- printf("0. Exit\n");
- printf("1. Add data\n");
+  printf("========= Welcome =========\n");
+  printf("0. Exit\n");
+  printf("1. Add data\n");
+  printf("2. pop element\n");
+  printf("3. add element at\n");
+  printf("4. print array\n");
 }
 
-void add_value(void) {
- DynamicArray arr;
- int value;
- printf("Enter a value : ");
- scanf("%d", &value);
+void add_value(DynamicArray *arr) {
 
- if(push_back(&arr, value)) {
-  printf("Data added successfully \n");
- }else {
-  printf("Simething went wrong\n");
- }
+  int value;
+  printf("Enter a value : ");
+  scanf("%d", &value);
+
+  if (push_back(arr, value)) {
+    printf("Data added successfully \n");
+    printf("Size = %d\n", arr->size);
+  } else {
+    printf("Simething went wrong\n");
+  }
 }
 
-void start_ui(void) {
- 
- int option;
- do{
- menu_option();
- scanf("%d", &option);
- switch(option) {
-  case 1:
-  add_value();
-  break;
-  default:
-  printf("Invalid option\n");
- }
- }while(option != 0);
+void add_element_at(DynamicArray *arr) {
+  int index;
+  int value;
 
+  printf("Enter index : \n");
+  scanf("%d", &index);
+
+  printf("Enter value : \n");
+  scanf("%d", &value);
+
+  if (insert_at(arr, index, value)) {
+    printf("element inserted successfully");
+  } else {
+    printf("Something went wrong");
+  }
+}
+
+void start_ui(DynamicArray *arr) {
+
+  int option;
+  do {
+    menu_option();
+    scanf("%d", &option);
+    switch (option) {
+    case 0:
+      printf("Exiting...\n");
+      break;
+
+    case 1:
+      add_value(arr);
+      break;
+    case 2:
+      pop_back(arr);
+      break;
+    case 3:
+      add_element_at(arr);
+      break;
+    case 4:
+      print_array(arr);
+      break;
+
+    default:
+      printf("Invalid option\n");
+    }
+  } while (option != 0);
 }
